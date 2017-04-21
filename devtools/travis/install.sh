de31e09a620713fb98149db0a0536e02be4cfa0f
David Slochower <slochower@gmail.com>
2017-04-21 16:58:56 -0700

    Bundled travis-ci requirements into devtools/travis/install.sh

---
+++ devtools/travis/install.sh
@@ -0,0 +1,20 @@
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh
+
+bash miniconda.sh -b
+
+export PATH=$HOME/miniconda/bin:$PATH
+conda update conda -y
+conda install --yes conda-build jinja2 binstar pip
+conda config --add channels omnia
+
+conda create -y -n myenv python=$PYTHON_VERSION \
+       numpy scipy pandas nose openmm coverage nose-timer \
+       python-coveralls netCDF4
+
+source activate myenv
+
+wget https://github.com/ParmEd/ParmEd/archive/2.7.3.tar.gz -O parmed.tar.gz
+tar xvfz parmed.tar.gz
+cd parmed
+python setup.py install
+cd ..
