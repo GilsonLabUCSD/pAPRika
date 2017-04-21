32fc8c15327750ba3da490f73b89874f23b6fe75
David Slochower <slochower@gmail.com>
2017-04-21 16:31:04 -0700

    Add versioneer versioning, add tests for alignment

--- paprika/setup/align.py
+++ paprika/setup/align.py
@@ -1,12 +1,11 @@
 import numpy as np
 import parmed as pmd
 
-def align(mask1, mask2, prmtop='../../test/cb6-but/vac.topo', inpcrd='../../test/cb6-but/vac.crds'):
+def align(structure, mask1, mask2, save=False, filename=None):
     """
     Align the mask1 -- mask2 vector to the z axis.
     """
 
-    structure = pmd.load_file(prmtop, inpcrd)
     mask1_coordinates = structure[mask1].coordinates
     mask1_masses = [atom.mass for atom in structure[mask1].atoms]
     mask1_com = pmd.geometry.center_of_mass(np.asarray(mask1_coordinates),
@@ -17,15 +16,15 @@ def align(mask1, mask2, prmtop='../../test/cb6-but/vac.topo', inpcrd='../../test
     mask2_com = pmd.geometry.center_of_mass(np.asarray(mask2_coordinates),
                                             np.asarray(mask2_masses))
 
-    print('Moving {} ({} atoms) to the origin...'.format(mask1, len(mask1_coordinates)))
-    print('Aligning {} ({} atoms) with the z axis...'.format(mask2, len(mask2_coordinates)))
-    
+    print('INFO: Moving {} ({} atoms) to the origin...'.format(mask1, len(mask1_coordinates)))
+    print('INFO: Aligning {} ({} atoms) with the z axis...'.format(mask2, len(mask2_coordinates)))
+
     z = np.array([0.0, 0.0, 1.0])
     I = np.identity(3)
     # https://math.stackexchange.com/questions/293116/rotating-one-3d-vector-to-another
     # 1. Align mask1 to the origin.
     mask2_com = mask2_com + -1.0 * mask1_com
-    
+
     # 2. Find axis and angle using cross and dot products.
     x     = np.cross(mask2_com, z) / np.linalg.norm(np.cross(mask2_com, z))
     theta = np.arccos(np.dot(mask2_com, z)/(np.linalg.norm(mask2_com) * np.linalg.norm(z)))
@@ -44,31 +43,47 @@ def align(mask1, mask2, prmtop='../../test/cb6-but/vac.topo', inpcrd='../../test
         aligned_coords[atom] = np.dot(rotation_matrix, aligned_coords[atom])
     structure.coordinates = aligned_coords
 
-    save = True
     if save:
-        # This seems to write out HETATM in place of ATOM
-        structure.write_pdb('tmp.pdb')
+        if not filename:
+            print('WARNING: Unable to save aligned coordinates (no filename provided)...')
+        else:
+            print('INFO: Saved aligned coordinates to {}'.format(filename))
+            # This seems to write out HETATM in place of ATOM
+            # We should offer the option of writing a mol2 file, directly.
+            structure.write_pdb(filename)
 
     return structure
     
-def check_coordinates(mask, file='tmp.pdb'):
+def check_coordinates(structure, mask):
     """ 
-    Return the coordinates of an atom selection. 
+    Return the coordinates of an atom selection.
     """
-    structure = pmd.load_file(file)
     mask_coordinates = structure[mask].coordinates
     mask_masses = [atom.mass for atom in structure[mask].atoms]
     mask_com = pmd.geometry.center_of_mass(np.asarray(mask_coordinates),
                                             np.asarray(mask_masses))
     return mask_com
 
-def offset_structure(structure, offset, file='tmp.pdb'):
+def offset_structure(structure, offset):
     """
     Return a structure whose atoms have been translated.
     """
-    structure = pmd.load_file(file)
     offset_coords = np.empty_like(structure.coordinates)
     for atom in range(len(structure.atoms)):
-        offset_coords[atom] = structure.coordinates[atoms] + offset
+        offset_coords[atom] = structure.coordinates[atom] + offset
     structure.coordinates = offset_coords
+    print('INFO: Added offset of {} to atomic coordinates...'.format(offset))
+    return structure
+
+def return_structure(filename):
+    """
+    Return structure object from file name.
+    """
+    # `parmed` can read both PDBs and
+    # .inpcrd/.prmtop files with the same function call.
+    try:
+        structure = pmd.load_file(filename)
+        print('INFO: Loaded {}...'.format(filename))
+    except:
+        print('ERROR: Unable to load file: {}'.format(filename))
     return structure
