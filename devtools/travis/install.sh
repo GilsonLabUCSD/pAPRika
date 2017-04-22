35f8d602e367510799a1d001e3b4e0e0b42242d2
David Slochower <slochower@gmail.com>
2017-04-21 17:25:09 -0700

    Fix paths for travis-ci (again)

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -1,16 +1,13 @@
-wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh
+wget http://repo.continuum.io/miniconda/Miniconda3-4.3.11-Linux-x86_64.sh -O miniconda.sh
 
 bash miniconda.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
 conda update conda -y
-conda install --yes conda-build pip
+conda install --yes conda-build pip numpy scipy pandas coverage
 conda config --add channels omnia
 
-conda create -y -n myenv python=$PYTHON_VERSION \
-       numpy scipy pandas nose openmm coverage nose-timer \
-       python-coveralls netCDF4
-
+conda create -y -n myenv python=3
 source activate myenv
 
 wget https://github.com/ParmEd/ParmEd/archive/2.7.3.tar.gz -O parmed.tar.gz
