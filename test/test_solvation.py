cecdf91ea4a043d7fd5ed096b25894672950aa4d
nhenriksen <shireham@gmail.com>
2017-04-26 13:56:14 -0700

    re-enabled align-then-solvate test

--- test/test_solvation.py
+++ test/test_solvation.py
@@ -22,15 +22,15 @@ class TestSolvate(unittest.TestCase):
                 bufferwater=waters)
         self.assertEqual(countresidues(filename='tleap_apr_solvate.in', directory='./cb6-but/', returnlist='WAT'), waters, msg='{}'.format(waters))
 
-    # def test_alignment_workflow(self):
-    #     """ Test that we can solvate CB6-BUT after alignment. """
-    #     cb6 = pmd.load_file('./cb6-but/vac.pdb')
-    #     logger.debug(cb6, ':CB6', ':BUT', save=True, filename='./cb6-but/tmp.pdb')
-    #     waters = np.random.randint(1000, 10000)
-    #     print('Trying to solvate with {} waters...'.format(waters))
-    #     solvate(tleapfile='./cb6-but/tleap.in', pdbfile='./cb6-but/tmp.pdb',
-    #             bufferwater=waters)
-    #     self.assertEqual(countresidues(filename='tleap_apr_solvate.in', directory='./cb6-but/',returnlist='WAT'), waters, msg='{}'.format(waters))
+     def test_alignment_workflow(self):
+         """ Test that we can solvate CB6-BUT after alignment. """
+         cb6 = pmd.load_file('./cb6-but/vac.pdb')
+         align(cb6, ':CB6', ':BUT', save=True, filename='./cb6-but/tmp.pdb')
+         waters = np.random.randint(1000, 10000)
+         print('Trying to solvate with {} waters...'.format(waters))
+         solvate(tleapfile='./cb6-but/tleap.in', pdbfile='tmp.pdb',
+                 bufferwater=waters)
+         self.assertEqual(countresidues(filename='tleap_apr_solvate.in', directory='./cb6-but/',returnlist='WAT'), waters, msg='{}'.format(waters))
 
 if __name__ == '__main__':
     unittest.main()
