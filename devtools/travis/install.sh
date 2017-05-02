2d1f285286fb6e0fa23af90c7bea1f075b064075
David Slochower <slochower@gmail.com>
2017-05-02 15:19:09 -0700

    Adding python 2.7 tests

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -1,17 +1,10 @@
-# wget http://repo.continuum.io/miniconda/Miniconda3-4.3.11-Linux-x86_64.sh -O miniconda.sh
-
-# bash miniconda.sh -b
 
 export PATH=$HOME/miniconda3/bin:$PATH
-conda update conda -y
-conda install --yes conda-build pip numpy scipy pandas coverage
-# conda config --add channels omnia
-# conda install --yes openmm
 
-# wget https://github.com/ParmEd/ParmEd/archive/2.7.3.tar.gz -O parmed.tar.gz
-# tar xvfz parmed.tar.gz
-# cd ParmEd-2.7.3
-# python setup.py install
-# cd ../
+conda create -y -n myenv python=$PYTHON_VERSION \
+      numpy scipy pandas
 
-conda install -y ambertools=17.0 -c http://ambermd.org/downloads/ambertools/conda/
+conda install -y -n myenv \
+      ambertools=17.0 -c http://ambermd.org/downloads/ambertools/conda/
+
+source activate myenv
