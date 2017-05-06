4f479f8380d1fcc536b85a96a41c616225601e10
David Slochower <slochower@gmail.com>
2017-05-06 09:27:45 -0700

    WIP: atom indexing for restraints. Indexing appears to be recycled for each mask (unclear). Windows converted into separate force and target dictionaries...

--- paprika/restraints.py
+++ paprika/restraints.py
@@ -44,37 +44,61 @@ class DAT_restraint(object):
                         'force_increment':   None
                        }
 
-        self.attach_windows  = []
-        self.pull_windows    = []
-        self.release_windows = []
+        self.attach_forces, self.attach_targets  = [], []
+        self.pull_forces, self.pull_targets    = [], []
+        self.release_forces, self.release_targets = [], []
+        
 
     def index_from_mask(self, mask):
         """
         Return the atom indicies, given a mask.
         """
         structure = align.return_structure(self.structure_file)
-        return [i.idx for i in structure[mask]]
+        masked = structure[mask]
+        log.debug('There are {} atoms in the mask...'.format(masked.atoms))
+        indices = [i.idx for i in masked]
+        log.debug('Found indices {} for mask {}...'.format(indices, mask))
+        return indices
 
     def initialize(self):
         """
         If the user hasn't already declared the windows list, we will
         construct one from the initial, final, and increment values.
         """
-        if not self.attach_windows and self.attach['force_initial']:
-            self.attach_windows = np.arange(self.attach['force_initial'],
-                                            self.attach['force_final'],
-                                            self.attach['force_increment'])
-        if not self.pull_windows and self.pull['force_final']:
-            self.pull_windows = np.arange(self.pull['force_initial'],
-                                          self.pull['force_final'],
-                                          self.pull['force_increment'])
-        if not self.release_windows and self.release['force_final']:
-            self.release_windows = np.arange(self.release['force_initial'],
+        if not self.attach_forces and self.attach['force_final']:
+            self.attach_forces = np.arange(self.attach['force_initial'],
+                                           self.attach['force_final'],
+                                           self.attach['force_increment'])
+            # Make a list of targets as long as the attachment windows.
+            self.attach_targets = self.attach['target'] * len(self.attach_forces)
+        if not self.pull_forces and self.pull['force_final']:
+            # In the pulling phase, the target distance also changes.
+            self.pull_targets = np.arange(self.pull['target_initial'],
+                                          self.pull['target_final'],
+                                          self.pull['target_increment'])
+
+            if self.pull['force_initial'] != self.pull['force_final']:
+                self.pull_forces = np.arange(self.pull['force_initial'],
+                                             self.pull['force_final'],
+                                             self.pull['force_increment'])
+            else:
+                self.pull_forces = [self.pull['force_initial']] * len(self.pull_targets)
+        if not self.release_forces and self.release['force_final']:
+            self.release_forces = np.arange(self.release['force_initial'],
                                             self.release['force_final'],
                                             self.release['force_increment'])
-        self.phase = {'attach':  self.attach_windows,
-                      'pull':    self.pull_windows,
-                      'release': self.release_windows
+            # Make a list of targets as long as the release windows.
+            self.release_targets = self.release['target'] * len(self.release_forces)
+
+        self.phase = {'attach':  {'forces' : self.attach_forces,
+                                  'targets' : self.attach_targets
+                                 },
+                      'pull':    {'forces' : self.pull_forces,
+                                  'targets' : self.pull_targets
+                                 },
+                      'release': {'forces' : self.release_forces,
+                                  'targets' : self.release_targets
+                                 }
                      }
 
         self.index1 = self.index_from_mask(self.mask1)
@@ -96,16 +120,18 @@ def return_restraint_line(restraint, phase, window, group=False):
     &rst iat= 3,109, r1= 0.0000, r2= 6.9665, r3= 6.9665, r4= 999.0000,
          rk2= 5.0000000, rk3= 5.0000000, &end
     """
-
+    # import pdb; pdb.set_trace()
     string = '&rst ' + \
-             '\tiat = {}, {}, {}, {},'.format(restraint.index1, restraint.index2, \
-                                              restraint.index3, restraint.index4) + \
+             '\tiat = {}, {}, {}, {},'.format([i for i in restraint.index1], \
+                                              [i for i in restraint.index2], \
+                                              [i for i in restraint.index3], \
+                                              [i for i in restraint.index4]) + \
              '\tr1  = {0:4.4f}'.format(0) + \
-             '\tr2  = {0:4.4f}'.format(restraint.phase[phase][window]) + \
-             '\tr3  = {0:4.4f}'.format(restraint.phase[phase][window]) + \
+             '\tr2  = {0:4.4f}'.format(restraint.phase[phase]['targets'][window]) + \
+             '\tr3  = {0:4.4f}'.format(restraint.phase[phase]['targets'][window]) + \
              '\tr4  = {0:4.4f}'.format(999) + \
-             '\trk2 = {0:4.4f}'.format(restraint.phase[phase] + \
-             '\trk3 = {0:4.4f}'.format(restraint.phase[phase] + \
+             '\trk2 = {0:4.4f}'.format(restraint.phase[phase]['forces'][window]) + \
+             '\trk3 = {0:4.4f}'.format(restraint.phase[phase]['forces'][window]) + \
              '\t&end'
 
     if group:
@@ -120,8 +146,8 @@ def return_restraint_line(restraint, phase, window, group=False):
 # Initialize a distance restraint that acts on :BUT and :CB6...
 this = DAT_restraint()
 this.structure_file = '../test/cb6-but/cb6-but.pdb'
-this.mask1 = ':BUT'
-this.mask2 = ':CB6'
+this.mask1 = ':BUT@C3'
+this.mask2 = ':CB6@C10'
 
 # User specifies the windows directly...
 # this.pull_windows = [0, 1.1, 2.4, 3.6]
@@ -134,7 +160,7 @@ this.pull = {'target_initial'   : 0,  # angstroms (by definition, I guess)
              'force_increment'  : 1   # kcal per mol (but shouldn't matter)
             }
 this.initialize()
-return_restraint_line(this, phase='pull', window=0)
+return_restraint_line(this, phase='pull', window=5)
 
 # Generate windows for each
 # for number, window in enumerate(this.pull_windows):
