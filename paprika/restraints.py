73360b134c365981ebc395fc6c4c6352122db674
David Slochower <slochower@gmail.com>
2017-05-31 18:08:23 -0700

    WIP: restraint formatting, needs to be reworked to include the six ways to specify restraints and also to include target values.

--- paprika/restraints.py
+++ paprika/restraints.py
@@ -7,7 +7,8 @@ from paprika import align
 
 logger = log.getLogger()
 logger.setLevel(log.DEBUG)
-log.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
+log.basicConfig(format='%(asctime)s %(message)s',
+                datefmt='%Y-%m-%d %I:%M:%S %p')
 
 
 # Page 379 of Amber 7 manual has mask specifications
@@ -27,25 +28,165 @@ class DAT_restraint(object):
         self.index3 = None
         self.index4 = None
 
-        self.attach =  {'target_initial':    None, # Percent of force constant
-                        'target_final':      None,
-                        'target_increment':  None,
-                        'force_constant':    None,
-                        'targets':           None, # List to hold actual target values
-                        'forces':            None  # List to hold actual force values
-                       }
-        self.pull =    {'target_initial':    None, # Distance for restraint
-                        'target_final':      None,
-                        'target_increment':  None,
-                        'force_constant' :   None,
-                        'targets':           None,  # List to hold actual target values
-                        'forces':            None   # List to hold actual force values
-                       }
-        self.release = {'target':            None,
-                        'force_initial':     None,
-                        'force_final':       None,
-                        'force_increment':   None
-                       }
+        self.attach = {
+            'target_values':    None,  # If the user wants to specify a list...
+            'target_initial':   None,  # Percent of force constant
+            'target_final':     None,
+            'target_increment': None,
+            'force_values':     None,  # If the user wants to specify a list...
+            'force_initial':    None,
+            'force_final':      None,
+            'force_increment':  None
+        }
+
+        self.pull = {
+            'target_values':    None,
+            'target_initial':   None,
+            'target_final':     None,
+            'target_increment': None,
+            'force_values':     None,
+            'force_initial':    None,
+            'force_final':      None,
+            'force_increment':  None
+        }
+
+        self.release = {
+            'target_values':    None,
+            'target_initial':   None,
+            'target_final':     None,
+            'target_increment': None,
+            'force_values':     None,
+            'force_initial':    None,
+            'force_final':      None,
+            'force_increment':  None
+        }
+        log.info("1. Specify a list of values like `restraint.attach['target_values']`")
+        log.info("2. Specify a range of values using `restraint.attach['target_initial]`, "
+                 "`restraint.attach['target_final']` and "
+                 "`restraint.attach['target_increment']` ")
+        log.info("3. Specify a single value like `restraint.attach['force_final']`")
+
+    def initialize(self):
+        """
+        If the user hasn't already declared the windows list, we will
+        construct one from the initial, final, and increment values.
+        """
+
+        # Create the container to hold the actual target and force values that
+        # are computed from the class lists. This is a roundabout way of keeping track
+        # of the data we need to calculate the work done by the restraints while allowing
+        # flexibility in how the user specifies the targets and force constants using the
+        # __init__ method. There is probably a more elegant way of keeping track of this, but
+        # this should work for now.
+
+        self.phase = {
+            'attach':  {
+                'force_constants': None,
+                'targets':         None
+            },
+            'pull':    {
+                'force_constants': None,
+                'targets':         None
+            },
+            'release': {
+                'force_constants': None,
+                'targets':         None
+            }
+        }
+        # ------------------------------------ ATTACH ------------------------------------ #
+        # Either the user specifies a direct list of target values during attachment...
+        if self.attach['target_values']:
+            log.debug('User directly specified list of target values for attachment...')
+            self.phase['attach']['targets'] = self.attach['target_values']
+        # ... or we build a list of targets using a range supplied.
+        elif (self.attach['target_initial'] is not None) and \
+             (self.attach['target_increment'] is not None) and \
+             (self.attach['target_final'] is not None):
+            log.debug('Building attachment targets from fractions...')
+            self.phase['attach']['targets'] = np.arange(self.attach['target_initial'],
+                                                        self.attach['target_final'] +
+                                                        self.attach['target_increment'],
+                                                        self.attach['target_increment'])
+        # After we have the list of targets, we calculate the force constant in each window during
+        # attachment. Either the user specifies the list of force constants
+        # directly...
+        if self.attach['force_values']:
+            log.debug('User directly specified list of force constants for attachment...')
+            self.phase['attach']['force_constants'] = self.attach['force_values']
+        # ...or we find the force constant in each window by multiplying the target fraction
+        # by the final force constant.
+        elif self.attach['force_final']:
+            log.debug('Building attachment force constants from fractions...')
+            self.phase['attach']['force_constants'] = [self.attach['force_final'] * percent
+                                                       for percent in self.phase['attach']['targets']]
+
+        # ------------------------------------ RELEASE ------------------------------------ #
+        # Since release is the inverse of attach, the process is identical.
+        # Either the user specifies a direct list of target values during release...
+        if self.release['target_values']:
+            log.debug('User directly specified list of target values for release...')
+            self.phase['release']['targets'] = self.release['target_values']
+        # ... or we build a list of targets using a range supplied.
+        elif (self.release['target_initial'] is not None) and \
+             (self.release['target_increment'] is not None) and \
+             (self.release['target_final'] is not None):
+            log.debug('Building release targets from fractions...')
+            self.phase['release']['targets'] = np.arange(self.release['target_initial'],
+                                                         self.release['target_final'] +
+                                                         self.release['target_increment'],
+                                                         self.release['target_increment'])
+        # After we have the list of targets, we calculate the force constant in each window during
+        # release. Either the user specifies the list of force constants
+        # directly...
+        if self.release['force_values']:
+            log.debug('User directly specified list of force constants for release...')
+            self.phase['release']['force_constants'] = self.release['force_values']
+        # ...or we find the force constant in each window by multiplying the target fraction
+        # by the final force constant.
+        elif self.release['force_final']:
+            log.debug('Building release force constants from fractions...')
+            self.phase['release']['force_constants'] = [self.release['force_final'] * percent
+                                                        for percent in self.phase['release']['targets']]
+
+        # ------------------------------------ PULL ------------------------------------ #
+        # Either the user specifies a direct list of target values for puling...
+        if self.pull['target_values']:
+            log.debug('User directly specified a list of target values for pulling...')
+            self.phase['pull']['targets'] = self.pull['target_values']
+        # ...or we build a list of targets using a range supplied.
+        elif (self.pull['target_initial'] is not None) and \
+             (self.pull['target_increment'] is not None) and \
+             (self.pull['target_final'] is not None):
+            log.debug('Building pull targets from fractions...')
+            self.phase['pull']['targets'] = np.arange(self.pull['target_initial'],
+                                                      self.pull['target_final'] +
+                                                      self.pull['target_increment'],
+                                                      self.pull['target_increment'])
+
+        # After we have the list of targets, we calculate the force constant in each window during
+        # release. Either the user specifies the list of force constants
+        # directly...
+        if self.pull['force_values']:
+            log.debug('User directly specified list of force constants for pull...')
+            self.phase['pull']['force_constants'] = self.pull['force_values']
+        # ...or we find the force constant in each window by multiplying the target fraction
+        # by the final force constant.
+        elif self.pull['force_final']:
+            log.debug('Building pull force constants from fractions...')
+            self.phase['pull']['force_constants'] = [self.pull['force_final'] * percent
+                                                     for percent in self.phase['pull']['targets']]
+
+        # ---------------------------------- ATOM MASKS ---------------------------------- #
+        self.index1 = self.index_from_mask(self.mask1)
+        self.index2 = self.index_from_mask(self.mask2)
+        if self.mask3:
+            self.index3 = self.index_from_mask(self.mask3)
+        else:
+            self.index3 = None
+        if self.mask4:
+            self.index4 = self.index_from_mask(self.mask4)
+        else:
+            self.index4 = None
 
     def index_from_mask(self, mask):
         """
@@ -58,86 +199,6 @@ class DAT_restraint(object):
         indices = [i.idx for i in masked]
         # log.debug('Found indices {} for mask {}...'.format(indices, mask))
         return indices
-
-    def initialize(self):
-        """
-        If the user hasn't already declared the windows list, we will
-        construct one from the initial, final, and increment values.
-        """
-        if not self.attach['targets']:
-            log.debug('Building attach targets from fractions...')
-            self.attach['targets'] = np.arange(self.attach['target_initial'],
-                                               self.attach['target_final'] +
-                                               self.attach['target_increment'],
-                                               self.attach['target_increment'])
-        if not self.attach['forces']:
-            log.debug('Building attach force targets from fractions...')
-            self.attach['forces'] = [self.attach['force_constant'] * i
-                                     for i in self.attach['targets']]
-
-        if not self.pull['targets']:
-            log.debug('Building pull targets from fractions...')
-            self.pull['targets'] = np.arange(self.pull['target_initial'],
-                                             self.pull['target_final'] +
-                                             self.pull['target_increment'],
-                                             self.pull['target_increment'])
-        if not self.pull['forces']:
-            log.debug('Building pull force targets from fractions...')
-            self.pull['forces'] = [self.pull['force_constant'] * i
-                                     for i in self.pull['targets']]
-
-
-
-        if not self.attach_targets and self.attach['force']:
-            log.debug('Building attach force targets from a range...')
-            # Make a list of targets as long as the attachment windows.
-            # I'm not sure the following line is working...
-            log.warn('Investigate!')
-            # self.attach_targets = [self.attach['target']] * len(self.attach_forces)
-
-        if not self.pull_forces and self.pull['force_final']:
-            # In the pulling phase, the target distance also changes.
-            self.pull_targets = np.arange(self.pull['target_initial'],
-                                          self.pull['target_final'] +
-                                          self.pull['target_increment'],
-                                          self.pull['target_increment'])
-
-            if self.pull['force_initial'] != self.pull['force_final']:
-                self.pull_forces = np.arange(self.pull['force_initial'],
-                                             self.pull['force_final'] +
-                                             self.pull['force_increment'],
-                                             self.pull['force_increment'])
-            else:
-                self.pull_forces = [self.pull['force_initial']] * len(self.pull_targets)
-        if not self.release_forces and self.release['force_final']:
-            self.release_forces = np.arange(self.release['force_initial'],
-                                            self.release['force_final'] +
-                                            self.release['force_increment'],
-                                            self.release['force_increment'])
-            # Make a list of targets as long as the release windows.
-            self.release_targets = [self.release['target']] * len(self.release_forces)
-
-        self.phase = {'attach':  {'forces' : self.attach_forces,
-                                  'targets' : self.attach_targets
-                                 },
-                      'pull':    {'forces' : self.pull_forces,
-                                  'targets' : self.pull_targets
-                                 },
-                      'release': {'forces' : self.release_forces,
-                                  'targets' : self.release_targets
-                                 }
-                     }
-
-        self.index1 = self.index_from_mask(self.mask1)
-        self.index2 = self.index_from_mask(self.mask2)
-        if self.mask3:
-            self.index3 = self.index_from_mask(self.mask3)
-        else:
-            self.index3 = None
-        if self.mask4:
-            self.index4 = self.index_from_mask(self.mask4)
-        else:
-            self.index4 = None
 
 
 def return_restraint_line(restraint, phase, window, group=False):
@@ -209,10 +270,10 @@ def return_restraint_line(restraint, phase, window, group=False):
             igr4 += '{}, '.format(index)
 
     string = '&rst ' + \
-            '\tiat = {}, {}, {}, {},'.format(iat1, \
-                                             iat2, \
-                                             iat3, \
-                                             iat4)
+             '\tiat = {}, {}, {}, {},'.format(iat1,
+                                              iat2,
+                                              iat3,
+                                              iat4)
     if group1:
         string += 'igr1 = {}'.format(igr1)
     if group2:
@@ -222,14 +283,15 @@ def return_restraint_line(restraint, phase, window, group=False):
     if group4:
         string += 'igr4 = {}'.format(igr4)
     string += \
-            '\tr1  = {0:4.4f},'.format(0) + \
-            '\tr2  = {0:4.4f},'.format(restraint.phase[phase]['targets'][window]) + \
-            '\tr3  = {0:4.4f},'.format(restraint.phase[phase]['targets'][window]) + \
-            '\tr4  = {0:4.4f},'.format(999) + \
-            '\trk2 = {0:4.4f},'.format(restraint.phase[phase]['forces'][window]) + \
-            '\trk3 = {0:4.4f},'.format(restraint.phase[phase]['forces'][window]) + \
-            '\t&end'
+        '\tr1  = {0:4.4f},'.format(0) + \
+        '\tr2  = {0:4.4f},'.format(restraint.phase[phase]['targets'][window]) + \
+        '\tr3  = {0:4.4f},'.format(restraint.phase[phase]['targets'][window]) + \
+        '\tr4  = {0:4.4f},'.format(999) + \
+        '\trk2 = {0:4.4f},'.format(restraint.phase[phase]['force_constants'][window]) + \
+        '\trk3 = {0:4.4f},'.format(restraint.phase[phase]['force_constants'][window]) + \
+        '\t&end'
     return string
+
 
 def make_directories(restraint):
     """
@@ -250,13 +312,15 @@ def make_directories(restraint):
     for window, _ in enumerate(restraint.pull_forces):
         os.makedirs('./windows/p{0:03d}'.format(window), exist_ok=True)
 
+
 def write_restraints_file(restraints, filename='restraints.in'):
     """
     Take all the restraints and write them to a file in each window.
     """
     for restraint in restraints:
         for window, _ in enumerate(restraint.attach_forces):
-            line = return_restraint_line(restraint, phase='attach', window=window)
+            line = return_restraint_line(
+                    restraint, phase='attach', window=window)
             # Using append mode is crucial for multiple restraints.
             directory = './windows/a{0:03d}'.format(window)
             f = open(directory + '/' + filename, 'a')
@@ -265,12 +329,14 @@ def write_restraints_file(restraints, filename='restraints.in'):
 
     for restraint in restraints:
         for window, _ in enumerate(restraint.pull_forces):
-            line = return_restraint_line(restraint, phase='pull', window=window)
+            line = return_restraint_line(
+                    restraint, phase='pull', window=window)
             # Using append mode is crucial for multiple restraints.
             directory = './windows/p{0:03d}'.format(window)
             f = open(directory + '/' + filename, 'a')
             f.write(line + '\n')
             f.close()
+
 
 def clean_restraints_file(restraints, filename='restraints.in'):
     """
@@ -278,12 +344,10 @@ def clean_restraints_file(restraints, filename='restraints.in'):
     """
     for restraint in restraints:
         for window, _ in enumerate(restraint.attach_forces):
-
             directory = './windows/a{0:03d}'.format(window)
             os.remove(directory + '/' + filename)
 
     for restraint in restraints:
         for window, _ in enumerate(restraint.pull_forces):
-
             directory = './windows/p{0:03d}'.format(window)
             os.remove(directory + '/' + filename)
