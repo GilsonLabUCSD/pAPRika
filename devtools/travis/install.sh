203401f33e4774e2d3511a6556201e3e572f978a
David Slochower <slochower@gmail.com>
2017-10-13 14:57:50 -0700

    Add OpenMM to Travis

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -1,6 +1,10 @@
 
 export PATH=$HOME/miniconda3/bin:$PATH
 
+conda config --add channels omnia --add channels conda-forge
+
+conda install openmm
+
 conda create -y -n myenv python=$PYTHON_VERSION \
       numpy scipy pandas
 
