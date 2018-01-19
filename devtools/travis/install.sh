c327a51b2444fc7abd3bda2ab5e97740dbf60136
David Slochower <slochower@gmail.com>
2018-01-19 14:34:35 -0800

    Accidentally committed `codecov` changes in `openmm` branch (#24)
    * Adding features for codecov
    
    * Still working on codecov
    
    * codecov troubleshooting

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -1,12 +1,12 @@
 
-export PATH=$HOME/miniconda3/bin:$PATH
+# export PATH=$HOME/miniconda3/bin:$PATH
 
 conda config --add channels omnia --add channels conda-forge
 
-conda install -y openmm
+conda create -y -n myenv python=$PYTHON_VERSION
 
-conda create -y -n myenv python=$PYTHON_VERSION \
-      numpy scipy pandas
+conda install -y -n myenv \
+      openmm numpy scipy pandas pytest pytest-cov codecov
 
 conda install -y -n myenv \
       ambertools=17.0 -c http://ambermd.org/downloads/ambertools/conda/
