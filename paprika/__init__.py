4055c589746a6b147435c4cb82ef3277ed9793de
David Slochower <slochower@gmail.com>
2018-01-19 16:41:13 -0800

    Fix import error for logging

--- paprika/__init__.py
+++ paprika/__init__.py
@@ -3,7 +3,7 @@ The pAPRika package sets up and performs attach-pull-release calculations.
 """
 
 from ._version import get_versions
-import logging as logging
+import logging as log
 import parmed as pmd
 
 __version__ = get_versions()['version']
@@ -11,8 +11,7 @@ del get_versions
 
 logger = log.getLogger()
 logger.setLevel(log.DEBUG)
-log.basicConfig(format='%(asctime)s %(message)s',
-                datefmt='%Y-%m-%d %I:%M:%S %p')
+log.basicConfig(
+    format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
 
 __all__ = ["align", "build", "restraints", "simulate"]
-
