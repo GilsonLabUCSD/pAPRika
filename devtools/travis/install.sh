9d34ddda03d8734ee95e0b15f89f1529b6b99287
David Slochower <slochower@gmail.com>
2018-01-24 13:09:16 -0800

    Change order of arguments for miniconda

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -9,7 +9,7 @@ else
             wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
       fi    
       chmod +x ./miniconda.sh
-      ./miniconda.sh -b -p -u $HOME/miniconda
+      ./miniconda.sh -b -u -p $HOME/miniconda
       export PATH="$HOME/miniconda/bin:$PATH"
       
       conda config --add channels omnia --add channels conda-forge
