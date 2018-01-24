ed836c31478ef8978b8cff4d850423dfcb6513a0
David Slochower <slochower@gmail.com>
2018-01-24 12:25:18 -0800

    Debugging Travis caching

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -8,7 +8,7 @@ else
       else
             wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
       fi    
-      bash miniconda.sh -b -p -u $HOME/miniconda.sh
+      ./miniconda.sh -b -p -u $HOME/miniconda
       export PATH="$HOME/miniconda/bin:$PATH"
       
       conda config --add channels omnia --add channels conda-forge
