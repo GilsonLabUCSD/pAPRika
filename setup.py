3722570ac56bf421c887a431e9170467224445cf
David Slochower <slochower@gmail.com>
2018-01-24 10:28:00 -0800

    Simply versioning

--- setup.py
+++ setup.py
@@ -1,52 +1,31 @@
 import os
 import sys
-import versioneer
+import re
 from distutils.core import setup
+import subprocess as sp
 
 if sys.version_info < (2, 7):
-    sys.stderr.write('You must have at least Python 2.7 for pAPRika to work '
-                     'correctly, although we have only tested with Python 3.0.\n')
+    sys.stderr.write(
+        'You must have at least Python 2.7 for pAPRika to work '
+        'correctly, although we have only tested with Python 3.0.\n')
     sys.exit(1)
 
-from distutils.command.clean import clean as Clean
-
-class CleanCommand(Clean):
-    """python setup.py clean
-    """
-    # lightly adapted from scikit-learn package
-    description = "Remove build artifacts from the source tree"
-
-    def _clean(self, folder):
-        for dirpath, dirnames, filenames in os.walk(folder):
-            for filename in filenames:
-                if (filename.endswith('.so') or filename.endswith('.pyd')
-                        or filename.endswith('.dll')
-                        or filename.endswith('.pyc')):
-                    os.unlink(os.path.join(dirpath, filename))
-            for dirname in dirnames:
-                if dirname == '__pycache__':
-                    shutil.rmtree(os.path.join(dirpath, dirname))
-
-    def run(self):
-        Clean.run(self)
-        if os.path.exists('build'):
-            shutil.rmtree('build')
-        self._clean('./')
-
-# paprika package and all its subpackages
-packages = ['paprika']
-
+version = '0.0.2'
+try:
+    # use git to find current version
+    git_describe = sp.check_output(["git", "describe", "--always"]).strip()
+    version = re.sub('-g[0-9a-f]*$', '', git_describe)
+except:
+    pass
 
 if __name__ == '__main__':
 
-    cmdclass = dict(clean=CleanCommand)
-    cmdclass.update(versioneer.get_cmdclass())
-    setup(name='pAPRika',
-          version=versioneer.get_version(),
-          description='Attach-pull-release free energy calculations',
-          author='',
-          author_email='',
-          url='',
-          license='',
-          packages=packages,
-          cmdclass=cmdclass)
+    setup(
+        name='pAPRika',
+        version=version,
+        description='Attach-pull-release free energy calculations',
+        author='',
+        author_email='',
+        url='',
+        license='',
+        packages='paprika')
