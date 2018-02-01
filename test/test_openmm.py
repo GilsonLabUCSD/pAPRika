34e41f7bfa95b82b700e6dd7d600240c198bd6c0
David Slochower <slochower@gmail.com>
2018-01-31 16:48:50 -0800

    Adds basic OpenMM testing.

--- test/test_openmm.py
+++ test/test_openmm.py
@@ -15,16 +15,20 @@ from paprika.restraints import *
 from paprika.utils import HAS_OPENMM
 
 
-@unittest.skipUnless(HAS_OPENMM, 'Cannot run OpenMM tests without OpenMM installed.')
+@unittest.skipUnless(HAS_OPENMM,
+                     'Cannot run OpenMM tests without OpenMM installed.')
 class TestOpenMM(unittest.TestCase):
     def test_minimization_finishes(self):
         """ Test that we can minimize CB6-BUT with OpenMM. """
 
-        simulation = OpenMM_GB_simulation()
-        simulation.topology = '../test/cb6-but/vac.topo'
-        simulation.min['platform'] = 'CPU'
-        simulation.min['coordinates'] = '../test/cb6-but/vac.crds'
-        result, system = simulation.minimize(save=False)
+        my_simulation = OpenMM_GB_simulation()
+        my_simulation.topology = '../test/cb6-but/vac.topo'
+        my_simulation.min['platform'] = 'CPU'
+        my_simulation.min['coordinates'] = '../test/cb6-but/vac.crds'
+
+        simulation, _ = my_simulation.setup_system(my_simulation.min)
+
+        result = my_simulation.minimize(simulation, save=False)
         state = result.context.getState(getEnergy=True)
         energy = state.getPotentialEnergy() / unit.kilocalories_per_mole
         self.assertAlmostEqual(energy, -827.9, places=1)
@@ -32,41 +36,47 @@ class TestOpenMM(unittest.TestCase):
     def test_soft_minimization(self):
         """ Test that we can minimize CB6-BUT with OpenMM, turning on interactions slowly. """
 
-        sim = OpenMM_GB_simulation()
-        sim.topology = '../test/cb6-but/vac.topo'
-        sim.min['platform'] = 'CPU'
-        sim.min['coordinates'] = '../test/cb6-but/vac.crds'
+        my_simulation = OpenMM_GB_simulation()
+        my_simulation.topology = '../test/cb6-but/vac.topo'
+        my_simulation.min['platform'] = 'CPU'
+        my_simulation.min['coordinates'] = '../test/cb6-but/vac.crds'
 
-        simulation, system = sim.setup_system(sim.min)
+        simulation, system = my_simulation.setup_system(my_simulation.min)
 
-        result = sim.turn_on_interactions_slowly(simulation, system)
+        result = my_simulation.turn_on_interactions_slowly(simulation, system)
         state = result.context.getState(getEnergy=True)
         energy = state.getPotentialEnergy() / unit.kilocalories_per_mole
         self.assertAlmostEqual(energy, -827.9, places=1)
 
-        # How do we get all CustomExternalForces?
+    def test_openmm_single_restraint(self):
+        """ Test that we can impose restraints with OpenMM. """
+        my_simulation = OpenMM_GB_simulation()
+        my_simulation.topology = '../test/cb6-but/vac.topo'
+        my_simulation.md['platform'] = 'CPU'
+        my_simulation.md['coordinates'] = '../test/cb6-but/vac.crds'
+        my_simulation.md['steps'] = 1000
 
-    # def test_openmm_restraint(self):
-    #     """ Test that we can impose restraints with OpenMM. """
-    #     simulation = OpenMM_GB_simulation()
-    #     simulation.topology = '../test/cb6-but/vac.topo'
-    #     simulation.md['platform'] = 'CPU'
-    #     simulation.md['coordinates'] = '../test/cb6-but/vac.crds'
-    #     simulation.md['steps'] = 100
+        simulation, system = my_simulation.setup_system(
+            my_simulation.md, seed=42)
 
-    #     restraint = DAT_restraint()
-    #     restraint.mask1 = ':BUT'
-    #     # Need a way to get the system here...
+        restraint = DAT_restraint()
+        restraint.structure_file = my_simulation.topology
+        restraint.mask1 = ':BUT'
+        restraint.mask2 = ':CB6'
+        restraint.attach['target'] = 3.0
+        restraint.attach['num_windows'] = 4
+        restraint.attach['fc_initial'] = 0.0
+        restraint.attach['fc_final'] = 3.0
+        restraint.auto_apr = False
+        restraint.initialize()
 
-    #  restrraint could/should be a list
+        my_simulation.add_openmm_restraints(
+            system, [restraint], phase='attach', window=3)
 
-    #     # setup_openmm_restraints(system?, restraint, phase='a', window=0)
-
-    #     result, system = simulation.run_md(save=False)
-    #     state = result.context.getState(getEnergy=True)
-    #     energy = state.getPotentialEnergy() / unit.kilocalories_per_mole
-    #     print(energy)
-    #     # self.assertAlmostEqual(energy, -827.9, places=1)
+        result = my_simulation.run_md(simulation, seed=42, save=False)
+        state = result.context.getState(getEnergy=True)
+        energy = state.getPotentialEnergy() / unit.kilocalories_per_mole
+        self.assertAlmostEqual(energy, -750.0, places=1)
 
 
 if __name__ == '__main__':
