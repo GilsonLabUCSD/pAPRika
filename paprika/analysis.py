f370e17af02fb6a63ea01e03d3965dbd0ca2413a
nhenriksen <shireham@gmail.com>
2018-02-12 17:32:04 -0800

    WIP: analysis

--- paprika/analysis.py
+++ paprika/analysis.py
@@ -13,36 +13,61 @@ def compute(rest_list, rest_data, method='MBAR'):
     #    k_n[pot_win] = len(rest_data[pot_win][0])
 
     #attach_restraints = [rest for rest in rest_list if rest.phase['attach']['force_constants'] is not None]
-    attach_bool = [rest.phase['attach']['force_constants'] is not None for rest in rest_list]
 
-    if any(attach_bool):
+    kB = 1.381e-23 * 6.022e23 / (4.184 * 1000.0) # Boltzmann constant in kJ/mol/K
+    temp = 298.15
+    beta = 1/(kB * temp) # beta
+
+
+    active_rest = [rest.phase['attach']['force_constants'] is not None for rest in rest_list]
+
+    if any(active_rest):
         for i,rest in enumerate(rest_list):
-            if attach_bool[i]:
-                sort_idxs = np.argsort(rest.phase['attach']['force_constants'])
+            if active_rest[i]:
+                reorder = np.argsort(rest.phase['attach']['force_constants'])
                 if rest.continuous_apr:
                     continuous_apr = True
                 break
-        NumWin = len(sort_idxs)
-        #if continuous_apr:
-        #    NumWin += 1
+        num_win = len(rest.phase['attach']['force_constants'])
+
+        N_k = np.zeros([num_win], np.int32)
+        for i in range(num_win):
+            N_k[i] = rest_data['attach'][reorder[i]][0].size
+        if continuous_apr:
+            N_k[-1] = rest_data['pull'][0][0].size
+        N_max = np.max(N_k)
 
         if method == 'MBAR':
-            u_kn = np.zeros([NumWin*NumWin], np.float64)
+            u_kln = np.zeros([num_win,num_win,N_max], np.float64)
         else:
-            u_kn = np.zeros([NumWin], np.float64)
+            u_kn = np.zeros([num_win], np.float64)
 
-        k = 0
-        print(NumWin,sort_idxs)
-        for pot_win in range(NumWin):
+        #k = 0
+        for pot_idx in range(num_win):
             if method == 'MBAR':
-                print(pot_win, sort_idxs[pot_win])
-                if continuous_apr and pot_win == NumWin-1:
-                    print(rest_list[0].phase['pull']['force_constants'][0],np.mean(rest_data['pull'][0][0]))
-                else:
-                    print(rest_list[0].phase['attach']['force_constants'][sort_idxs[pot_win]],np.mean(rest_data['attach'][sort_idxs[pot_win]]))
-                #for crd_win in range(NumWin):
-                #    u_kn[k] =
-        
+                #if continuous_apr and pot_win == num_win-1:
+                #    print(rest_list[0].phase['pull']['force_constants'][0],np.mean(rest_data['pull'][0][0]))
+                #else:
+                #    print(rest_list[0].phase['attach']['force_constants'][sort_idxs[pot_win]],np.mean(rest_data['attach'][sort_idxs[pot_win]][0]))
+                
+                pass
+
+#                for crd_idx in range(num_win):
+#                    for rest_idx,rest in enumerate(rest_list):
+#                        if active_rest[i]:
+#                            if rest.mask3 is not None and rest.mask4 is not None:
+#                                target = rest.phase['attach']['targets'][reorder[pot_idx]]
+#                                bool_list = rest_data['attach'][reorder[crd_idx]][rest_idx] < target - 180.0
+#                                rest_data['attach'][reorder[crd_idx]][rest_idx][bool_list] += 360.0
+#                                bool_list = rest_data['attach'][reorder[crd_idx]][rest_idx] > target + 180.0
+#                                rest_data['attach'][reorder[crd_idx]][rest_idx][bool_list] -= 360.0
+#                    u_kln[k,l,0:N_k[k]] += np.sum(
+#                        beta*
+#                        rest_list[active_rest].phase['attach']['force_constants'][reorder[pot_idx]]*
+#                        (rest_data['attach'][reorder[crd_idx]][active_rest] - 
+#                        rest_list[active_rest].phase['attach']['targets'][reorder[pot_idx]])**2
+#                        )
+#        
         
     # If attach exists
         # establish attach_windows based on continuous_apr
