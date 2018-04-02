5a6c2cb4835c214823141b8962f7d8a9852c7e79
David Slochower <slochower@gmail.com>
2018-04-02 15:06:19 -0700

    Remove automatic setting of the root logger

--- paprika/__init__.py
+++ paprika/__init__.py
@@ -8,8 +8,8 @@ import re as re
 
 from paprika.version import find_version
 
-logger = log.getLogger()
-logger.setLevel(log.DEBUG)
-log.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
+# logger = log.getLogger()
+# logger.setLevel(log.DEBUG)
+# log.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
 
 __version__ = find_version()
