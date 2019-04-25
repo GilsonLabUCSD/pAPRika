1bdf37cdb747d1668963a8c4fdda6270a1380fed
David Slochower <slochower@gmail.com>
2019-04-25 11:23:37 -0700

    Update test_align

--- paprika/tests/test_align.py
+++ paprika/tests/test_align.py
@@ -3,12 +3,13 @@ Tests the alignment of residues to the z axis.
 """
 
 import os
+import pytest
 from paprika.align import *
 
 
 def test_center_mask():
     """ Test that the first mask is centered """
-    cb6 = pmd.load_file(os.path.join(os.path.dirname(__file__), "../data/zz/vac.pdb"))
+    cb6 = pmd.load_file(os.path.join(os.path.dirname(__file__), "../data/cb6-but/vac.pdb"))
     aligned_cb6 = zalign(cb6, ":CB6", ":BUT")
     test_coordinates = check_coordinates(aligned_cb6, ":CB6")
     assert np.allclose(test_coordinates, np.zeros(3))
@@ -29,3 +30,5 @@ def test_theta_after_alignment():
     cb6 = pmd.load_file(os.path.join(os.path.dirname(__file__), "../data/cb6-but/vac.pdb"))
     aligned_cb6 = zalign(cb6, ":CB6", ":BUT")
     assert get_theta(aligned_cb6, ":CB6", ":BUT", axis="z") == 0
+    assert pytest.approx(get_theta(aligned_cb6, ":CB6", ":BUT", axis="x"), 0.001) == 1.5708
+    assert pytest.approx(get_theta(aligned_cb6, ":CB6", ":BUT", axis="y"), 0.001) == 1.5708
