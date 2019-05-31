ddf2624aab7dcd7c918ccdccb9e819f7d4f10c15
David Slochower <slochower@gmail.com>
2019-05-30 18:00:46 -0700

    Loading static restraints from YAML works

--- paprika/setup.py
+++ paprika/setup.py
@@ -5,15 +5,13 @@ This class is going to contain the simulation setup...
 import pkg_resources
 
 from pathlib import Path
+from paprika.restraints import static_DAT_restraint, DAT_restraint
 from paprika.restraints.read_yaml import read_yaml
 from paprika.restraints import DAT_restraint,static_DAT_restraint
 
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-# global installed_benchmarks
 
 def _get_installed_benchmarks():
     _installed_benchmarks = {}
@@ -28,55 +26,79 @@ class Setup(object):
     The Setup class provides a wrapper function around the preparation of the host-guest system and the application of restraints.
     """
 
-    def __init__(self, host, guest):
+    def __init__(self, host, guest, backend="openmm"):
         self.host = host
         self.guest = guest
+        self.backend = backend
         self.conformational_restraints = False
-        self.backend = "amber"
+
 
         self.directory = "benchmarks"
 
-        self.parse_yaml()
+        if self.backend == "amber":
+            raise NotImplementedError
+
+        installed_benchmarks = get_benchmarks()
+        host_yaml, guest_yaml = self.parse_yaml(installed_benchmarks)
         self.build()
-        self.initialize_restraints()
+        self.initialize_restraints(host_yaml, guest_yaml)
         self.initialize_calculation()
+
+
+    def parse_yaml(self, installed_benchmarks):
+        """
+        Read the YAML recipe for the host and guest.
+
+        Returns
+        -------
+
+        """
+        try:
+            host_yaml = installed_benchmarks["host_guest_pairs"][self.host]["yaml"]
+        except KeyError:
+            logger.error(f"Cannot find YAML recipe for host: {self.host}")
+            logger.debug(installed_benchmarks)
+            raise FileNotFoundError
+        try:
+            guest_yaml = installed_benchmarks["host_guest_pairs"][self.host][self.guest]
+        except KeyError:
+            logger.error(f"Cannot find YAML recipe for guest: {self.guest}")
+            logger.debug(installed_benchmarks)
+            raise FileNotFoundError
+        return host_yaml, guest_yaml
 
     def build(self):
         """
         This is going to change to accomodate SMIRNOFF99Frosst.
+
         Returns
         -------
         """
-    pass
+        pass
 
-    def initialize_restraints(self):
+    def initialize_restraints(self, host_yaml, guest_yaml):
 
-        installed_benchmarks = _get_installed_benchmarks()
-        logger.debug(installed_benchmarks)
-        try:
-            host_yaml = installed_benchmarks["host_guest_pairs"][self.host]["yaml"]
-        except KeyError:
-            print(f"Cannot find YAML recipe for host: {self.host}")
-            print(installed_benchmarks)
-            raise
-        try:
-            guest_yaml = installed_benchmarks["host_guest_pairs"][self.host][self.guest]
-        except KeyError:
-            print(f"Cannot find YAML recipe for guest: {self.guest}")
-            print(installed_benchmarks)
-            raise
 
         self.host_yaml = read_yaml(host_yaml)
+        self.guest_yaml = read_yaml(guest_yaml)
+        windows = [self.host_yaml["calculation"]["windows"]["attach"],
+                   self.host_yaml["calculation"]["windows"]["pull"],
+                   self.host_yaml["calculation"]["windows"]["release"]
+        ]
+        structure = self.guest_yaml["complex"]
 
-        build()
-        solvate()
+        # It appears that ParmEd cannot currently read a PosixPath object, so I am going to
+        # convert these into strings.
 
+        static_restraints = []
         for restraint in self.host_yaml["calculation"]["restraints"]["static"]:
-            r = restraints.static_DAT_restraint(restraint_mask_list=[D1, H1],
+            r = static_DAT_restraint(restraint_mask_list=restraint["restraint"]["atoms"].split(),
                                                 num_window_list=windows,
-                                                ref_structure=structure,
-                                                force_constant=5.0,
-                                                amber_index=True)
+                                                ref_structure=str(guest_yaml.parent.joinpath(structure)),
+                                                force_constant=restraint["restraint"]["force_constant"],
+                                                amber_index=False if self.backend == "openmm" else True
+                                     )
+            static_restraints.append(r)
 
         # for conformational in self.restraints_dictionary["calculation"]["restraints"]["conformational"]:
         #     raise NotImplementedError
@@ -122,3 +144,13 @@ class Setup(object):
             pass
 
         # Should the end point of this class be a file that contains restraints as JSON and either an AMBER input or serialized OpenMM system?
+
+
+
+def get_benchmarks():
+    """
+    Determine the installed benchmarks.
+
+    """
+    installed_benchmarks = _get_installed_benchmarks()
+    return installed_benchmarks
