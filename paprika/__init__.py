f7522de6f18ccaf05fb638130801fd82be6a31c2
David Slochower <slochower@gmail.com>
2019-07-31 10:54:47 -0700

    Try to prevent failures with `paprika.setup()` imports from __init__ when OpenMM is missing

--- paprika/__init__.py
+++ paprika/__init__.py
@@ -2,23 +2,35 @@
 pAPRika
 Advanced toolkit for binding free energy calculations
 """
+import logging
 
 # Make Python 2 and 3 imports work the same
 # Safe to remove with Python 3-only code
 from __future__ import absolute_import
 
-# Add imports here
-
 # Handle versioneer
 from ._version import get_versions
+
+# Add imports here
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
 
-__all__ = ["setup", "analyze"]
 
-from paprika.setup import Setup
+logger = logging.getLogger(__name__)
+
+try:
+    from simtk import openmm
+    from paprika.setup import Setup
+
+    setup = Setup
+except ImportError as e:
+    logging.info("OpenMM not found.")
+    logging.info("`paprika.setup()` requires OpenMM.")
+    setup = None
+
 from paprika.analyze import Analyze
-setup = Setup
 analyze = Analyze
+
+__all__ = ["setup", "analyze"]
