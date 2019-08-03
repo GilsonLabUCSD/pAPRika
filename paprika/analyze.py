0d8bccef279ce70a020a08deea3c24dbf0bcc464
David Slochower <slochower@gmail.com>
2019-08-03 13:03:19 -0700

    Support cases where the guest residue name is not the same as self.guest()

--- paprika/analyze.py
+++ paprika/analyze.py
@@ -4,6 +4,7 @@ This class contains a simulation analysis wrapper for use with the Property Esti
 
 import logging
 from pathlib import Path
+import parmed as pmd
 
 import numpy as np
 from paprika.analysis import fe_calc
@@ -17,13 +18,17 @@ class Analyze(object):
     The Analyze class provides a wrapper function around the analysis of simulations.
     """
 
-    def __init__(self, host, guest=None, restraint_file="restraints.json",
+    def __init__(self, host, guest=None, guest_orientation=None, restraint_file="restraints.json",
                  topology_file='coordinates.pdb', trajectory_mask='*.dcd', directory_path="benchmarks",
-                 symmetry_correction=None):
+                 symmetry_correction=None, guest_residue_name=None):
 
         self.host = host
         self.guest = guest if guest is not None else "release"
-        self.directory = Path(directory_path).joinpath(self.host).joinpath(self.guest)
+        self.guest_residue_name = self.guest if not guest_residue_name else guest_residue_name
+
+        self.directory = Path(directory_path).joinpath(self.host).joinpath(f"{self.guest}-{guest_orientation}" if
+                                                                           guest_orientation is not None else
+                                                                           f"{self.guest}")
 
         self.restraints = load_restraints(self.directory.joinpath(restraint_file))
         self.results = self.analyze(topology_file, trajectory_mask).results
@@ -49,20 +54,31 @@ class Analyze(object):
         if self.guest != "release":
             analysis.compute_free_energy(phases=["attach", "pull"])
 
+            structure = pmd.load_file(str(analysis.path.joinpath("a000").joinpath(analysis.prmtop)))
+            r_restraint = None
+            beta_restraint = None
+            theta_restraint = None
+
             for restraint in self.restraints:
-                if "DM" in restraint.mask1 and self.guest.upper() in restraint.mask2 and not restraint.mask3 and \
+
+                mask2_residue_name = structure[restraint.mask2].residues[0].name
+
+                if "DM" in restraint.mask1 and self.guest_residue_name.upper() in mask2_residue_name and not restraint.mask3 and \
                         not restraint.mask4:
                     r_restraint = restraint
                 if restraint.mask3 and not restraint.mask4:
-                    if "DM" in restraint.mask1 and "DM" in restraint.mask2 and self.guest.upper() in restraint.mask3:
+                    mask3_residue_name = structure[restraint.mask3].residues[0].name
+                    if "DM" in restraint.mask1 and "DM" in restraint.mask2 and self.guest_residue_name.upper() in mask3_residue_name:
                         theta_restraint = restraint
-                    if "DM" in restraint.mask1 and self.guest.upper() in restraint.mask2 and self.guest.upper() in \
-                            restraint.mask3:
+                    if "DM" in restraint.mask1 and self.guest_residue_name.upper() in mask2_residue_name and self.guest_residue_name.upper() in \
+                            mask3_residue_name:
                         beta_restraint = restraint
-
-            analysis.compute_ref_state_work(
-                [r_restraint, theta_restraint, None, None, beta_restraint, None]
-            )
+            if r_restraint and theta_restraint and beta_restraint:
+                analysis.compute_ref_state_work(
+                    [r_restraint, theta_restraint, None, None, beta_restraint, None]
+                )
+            else:
+                logging.warning("Could not determine the r, θ, or β restraint for computing the analytic release step.")
 
         else:
             analysis.compute_free_energy(phases=["release"])
