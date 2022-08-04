fix units

paprika/build/align.py
@@ -2,7 +2,7 @@ import logging
 
 import numpy as np
 import parmed as pmd
-from openff.units import unit as pint_unit
+from openff.units import unit as openff_unit
 from paprika.utils import check_unit
 
 logger = logging.getLogger(__name__)
@@ -190,7 +190,7 @@ def rotate_around_axis(structure, axis, angle):
     axis = _return_array(axis)
 
     # Convert angle to radians (temporary until Pint integration)
-    angle = check_unit(angle, base_unit=pint_unit.radians).to(pint_unit.radians).magnitude
+    angle = check_unit(angle, base_unit=openff_unit.radians).to(openff_unit.radians).magnitude
 
     if np.array_equal(axis, np.array([1.0, 0.0, 0.0])):
         rotation_matrix = np.array(
@@ -300,7 +300,7 @@ def get_theta(structure, mask1, mask2, axis):
         np.dot(vector, axis) / (np.linalg.norm(vector) * np.linalg.norm(axis))
     )
 
-    return pint_unit.Quantity(theta, units=pint_unit.radians)
+    return openff_unit.Quantity(theta, units=openff_unit.radians)
 
 
 def get_rotation_matrix(vector, ref_vector):
@@ -462,9 +462,9 @@ def offset_structure(structure, offset, dimension=None):
     else:
         mask = _return_array(dimension)
 
-    offset = check_unit(offset, base_unit=pint_unit.angstrom)
+    offset = check_unit(offset, base_unit=openff_unit.angstrom)
     offset *= mask
-    offset = offset.to(pint_unit.angstrom).magnitude
+    offset = offset.to(openff_unit.angstrom).magnitude
 
     # Offset coordinates
     offset_coords = np.empty_like(structure.coordinates)

paprika/utils.py
@@ -7,7 +7,7 @@ from functools import lru_cache
 import numpy as np
 import parmed as pmd
 import pytraj as pt
-from openff.units import unit
+from openff.units import unit as openff_unit
 from parmed.structure import Structure as ParmedStructureClass
 
 logger = logging.getLogger(__name__)
@@ -321,31 +321,31 @@ def is_file_and_not_empty(file_path):
 
 def check_unit(variable, base_unit):
     """
-    Run a check if variable is a float or unit.Quantity. If float, assign
-    the unit of ``base_unit``, if unit.Quantity, check the dimensionality.
+    Run a check if variable is a float or openff_unit.Quantity. If float, assign
+    the unit of ``base_unit``, if openff_unit.Quantity, check the dimensionality.
 
-    variable: float or unit.Quantity
-    base_unit: unit.Quantity
+    variable: float or openff_unit.Quantity
+    base_unit: openff_unit.Quantity
     """
     quantity = variable
     if isinstance(variable, float):
-        quantity = unit.Quantity(variable, units=base_unit)
-    elif isinstance(variable, unit.Quantity):
+        quantity = openff_unit.Quantity(variable, units=base_unit)
+    elif isinstance(variable, openff_unit.Quantity):
         assert variable.dimensionality == base_unit.dimensionality
     elif isinstance(variable, list):
         if all(isinstance(x, float) for x in variable):
-            quantity = unit.Quantity(variable, units=base_unit)
-        elif all(isinstance(x, unit.Quantity) for x in variable):
+            quantity = openff_unit.Quantity(variable, units=base_unit)
+        elif all(isinstance(x, openff_unit.Quantity) for x in variable):
             assert all(
                 variable[x].dimensionality == base_unit.dimensionality for x in variable
             )
         else:
             raise KeyError(
-                "Please make my life easier by either specifying a list of all float or all unit.Quantity."
+                "Please make my life easier by either specifying a list of all float or all openff.unit.Quantity."
             )
     elif isinstance(variable, np.ndarray):
-        quantity = unit.Quantity(variable, units=base_unit)
+        quantity = openff_unit.Quantity(variable, units=base_unit)
     else:
-        raise KeyError("``variable`` should be a float or unit.Quantity.")
+        raise KeyError("``variable`` should be a float or openff.unit.Quantity.")
 
     return quantity